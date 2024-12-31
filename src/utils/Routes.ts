ea2a0b35481a0f381fd005a4698e05d972368f96
unknown <nikitakabo74@gmail.com>
2024-12-31 13:22:10 +0300

    chore: update lib source url

--- src/utils/Routes.ts
+++ src/utils/Routes.ts
@@ -2,5 +2,5 @@ export const Routes = {
   main: "/",
   reference: "/reference/",
   docs: "/docs",
-  source: "https://github.com/Ayomits/ayocord",
+  source: "https://github.com/ayocord-js/ayocord.git",
 };
